d887ecfd0a22bb2b971bad3e9421b302a43bf137
Crowdbotics <team@crowdbotics.com>
2023-06-28 10:28:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "healthy-diet-41783",
+  url: "https://healthy-diet-41783.botics.co",
+  api: "https://healthy-diet-41783.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
